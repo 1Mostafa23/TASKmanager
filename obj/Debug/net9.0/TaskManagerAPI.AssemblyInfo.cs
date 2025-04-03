bdf9bbf7c3f118eff1a675255a341cfcc0db5640
v5.0 MYSTIC <mm0651225@gmail.com>
2025-04-04 00:12:53 +0300

    updated commit

--- obj/Debug/net9.0/TaskManagerAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskManagerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e67f1f7992e8971e6af3692c853638f6cf71871f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d2e76411b6af8e0241f20d5446598488625ca67")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
